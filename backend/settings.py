e80d4978fbd86f48562bbaf61e70aba4130d9048
sonalranee <sonal.ranee@gmail.com>
2026-04-09 21:12:02 +0400

    Fixed Django setup and server running

--- backend/settings.py
+++ backend/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
 
-INSTALLED_APPS = [
     'users',
     'stores',
     'products',
