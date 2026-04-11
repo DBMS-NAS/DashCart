ceb4fb242993a6c52f669e59665cace9fbc1107a
sonalranee <sonal.ranee@gmail.com>
2026-04-11 16:52:16 +0400

    Added JWT login + custom user roles + frontend integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
-<Route path="/login" element={<Login />} />
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
